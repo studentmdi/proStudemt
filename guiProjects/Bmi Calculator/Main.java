92f23b862aca8c50942e38b26eba75801bc38cb0
Lamin Jatta <ljatta64@yahoo.co>
2019-07-30 11:12:47 +0000

    my Gui Projects

--- guiProjects/Bmi Calculator/Main.java
+++ guiProjects/Bmi Calculator/Main.java
@@ -10,13 +10,13 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage main) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/application/bmi.fxml"));
 			Scene scene = new Scene(root,650,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			main.setScene(scene);
+			main.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
